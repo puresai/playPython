8f07ef465095a1f02db834ce330351afce9deb5b
13sai <957042781@qq.com>
2018-01-11 22:18:36 +0800

    20180111

--- common/commonDef.py
+++ common/commonDef.py
@@ -2,17 +2,21 @@ import urllib.request as urllib
 from urllib.request import urlretrieve
 from bs4 import BeautifulSoup
 import socket
+import pdb
 
 # 爬取页面
-def urlOpen(url, headers, ip = '', charset = 'utf-8'):
+def urlOpen(url, header, ip = '', charset = 'utf-8'):
 	if(ip == ''):
 		ip = getIp()
 	proxies = {'http' : ip}
 	proxy_support = urllib.ProxyHandler(proxies)
 	opener = urllib.build_opener(proxy_support)
 	urllib.install_opener(opener)
-	req = urllib.Request(url, headers=headers)
+	req = urllib.Request(url, headers=header)
+	print(1)
+	pdb.set_trace()
 	iUrl = urllib.urlopen(req).read().decode(charset)
+
 	return BeautifulSoup(iUrl, "html.parser")
 
 # 爬取图片
