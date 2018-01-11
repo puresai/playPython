54583f2eb21d35c469a2d8e92f2f391a58aa9f06
13sai <957042781@qq.com>
2018-01-11 19:06:04 +0800

    2018/01/11

--- common/commonDef.py
+++ common/commonDef.py
@@ -1,11 +1,25 @@
+import urllib.request as urllib
+from urllib.request import urlretrieve
+from bs4 import BeautifulSoup
+import socket
+
 # 爬取页面
-def urlOpen(url, charset = 'utf-8'):
-	from urllib.request import urlopen 
-	from bs4 import BeautifulSoup
-	iUrl = urlopen(url).read().decode(charset)
+def urlOpen(url, headers, ip = '', charset = 'utf-8'):
+	if(ip == ''):
+		ip = getIp()
+	proxies = {'http' : ip}
+	proxy_support = urllib.ProxyHandler(proxies)
+	opener = urllib.build_opener(proxy_support)
+	urllib.install_opener(opener)
+	req = urllib.Request(url, headers=headers)
+	iUrl = urllib.urlopen(req).read().decode(charset)
 	return BeautifulSoup(iUrl, "html.parser")
 
 # 爬取图片
 def getImg(imgUrl, path):
-	from urllib.request import urlretrieve
-	urlretrieve(imgUrl, path)
+	urllib.urlretrieve(imgUrl, path)
+
+# 获取IP
+def getIp():
+	myname = socket.getfqdn(socket.gethostname())
+	return socket.gethostbyname(myname)
