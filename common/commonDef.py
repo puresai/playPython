b6778bec658ac3a138d2d68215601916fd0b6ddf
13sai <957042781@qq.com>
2018-01-16 16:47:49 +0800

    modify

--- common/commonDef.py
+++ common/commonDef.py
@@ -1,29 +1,49 @@
-import urllib.request as urllib
+import urllib.request as request
 from urllib.request import urlretrieve
 from bs4 import BeautifulSoup
 import socket
 import pdb
 
-# 爬取页面
-def urlOpen(url, header, ip = '', charset = 'utf-8'):
-	if(ip == ''):
-		ip = getIp()
-	proxies = {'http' : ip}
-	proxy_support = urllib.ProxyHandler(proxies)
-	opener = urllib.build_opener(proxy_support)
-	urllib.install_opener(opener)
-	req = urllib.Request(url, headers=header)
-	print(1)
-	pdb.set_trace()
-	iUrl = urllib.urlopen(req).read().decode(charset)
+# BeautifulSoup转换
+def soup(str, code = "html.parser"):
+	return BeautifulSoup(str, code)
 
-	return BeautifulSoup(iUrl, "html.parser")
+# 爬取页面
+def urlOpen(url, charset = "utf-8", header = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"}):
+	req = request.Request(url, headers=header)
+	res = request.urlopen(req).read().decode(charset)
+	return soup(res)
 
 # 爬取图片
 def getImg(imgUrl, path):
-	urllib.urlretrieve(imgUrl, path)
+	urlretrieve(imgUrl, path)
 
 # 获取IP
 def getIp():
 	myname = socket.getfqdn(socket.gethostname())
 	return socket.gethostbyname(myname)
+
+# 代理IP访问页面
+def proxy(url, ip, charset = "utf-8"):
+	httpproxy_handler = request.ProxyHandler({"http" : ip, "https" : ip})
+	opener = request.build_opener(request.HTTPHandler, httpproxy_handler)
+	req = request.Request(url)
+	request.install_opener(opener)
+	response = opener.open(req)
+	return soup(response.read().decode(charset))
+
+# 验证IP有效性
+def checkIP(ip, url = "http://httpbin.org/get"):
+	try:
+		return proxy(url, ip)
+	except:
+		return False
+
+# 写文件
+def writeFile(content, path, type = 'a'):
+	try:
+		with open(path, type) as f:
+			f.write(content)
+	finally:
+		if f:
+			f.close()
